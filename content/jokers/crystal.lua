63aaff2f97af046e3ef9ad1bc750db8801a1a623
MetaNite64 <noahtrivedi@gmail.com>
2025-08-23 20:49:08 -0500

    (Joker) implement Crystal

---
+++ content/jokers/crystal.lua
@@ -0,0 +1,43 @@
+SMODS.Joker {
+  key = "crystal",
+  atlas = "placeholder",
+  pos = { x = 0, y = 0 },
+  config = { extra = {
+    retriggers = 3
+  } },
+  rarity = 3,
+  blueprint_compat = false,
+  eternal_compat = true,
+  perishable_compat = true,
+
+  loc_txt = { ['en-us'] = {
+    name = 'Crystal',
+    text = {
+      'Retrigger the {C:attention}Joker{} to',
+      'the right #1# times'
+    }
+  } },
+
+  loc_vars = function(self, info_queue, card)
+    return { vars = {
+      card.ability.extra.retriggers
+    } }
+  end,
+
+  calculate = function(self, card, context)
+    if context.retrigger_joker_check and context.other_card ~= card and not context.blueprint then
+      local other_joker = nil
+      for i = 1, #G.jokers.cards do
+        if G.jokers.cards[i] == card then
+          other_joker = G.jokers.cards[i + 1]
+          break
+        end
+      end
+      if other_joker == context.other_card then
+        return {
+          repetitions = card.ability.extra.retriggers
+        }
+      end
+    end
+  end
+}
