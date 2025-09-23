b2b26594658d65c60c99cf973eb45eef835e4fee
MetaNite64 <noahtrivedi@gmail.com>
2025-09-22 21:22:45 -0500

    (patches) unnecessary code cleanup

--- lib/patch.lua
+++ lib/patch.lua
@@ -1,4 +1,3 @@
-STAR_UTIL.Patches = {}
 STAR_UTIL.Patch = SMODS.Sticker:extend {
   prefix_config = { key = true },
   should_apply = false,
@@ -7,10 +6,6 @@ STAR_UTIL.Patch = SMODS.Sticker:extend {
   sets = {
     Default = true
   },
-
-  apply = function(self, card, val)
-    card.ability[self.key] = val and copy_table(self.config) or nil
-  end,
 
   draw = function(self, card)
     G.shared_stickers[self.key].role.draw_major = card
