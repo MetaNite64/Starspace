1e80098548403d70a4129ed36b5ce73c2a88e1a3
MetaNite64 <noahtrivedi@gmail.com>
2025-10-11 00:33:42 -0500

    put a random friend of astro on the title screen

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -1,4 +1,4 @@
--- init_game_object hook
+-- init_game_object hook: see enclosed comments
 local igo_ref = Game.init_game_object
 function Game.init_game_object(self)
   local ret = igo_ref(self)
@@ -16,6 +16,58 @@ function Game.init_game_object(self)
     secret_hands = secrets
   }
 
+  return ret
+end
+
+-- main_menu hook: add a random friend of astro to the title screen
+local gmm_ref = Game.main_menu
+function Game:main_menu(change_context)
+  local ret = gmm_ref(self, change_context)
+
+  -- get a table of all the friends of astro
+  local friends = {}
+  for i, v in pairs(SMODS.Centers) do
+    if v.pools and v.pools.friends_of_astro then table.insert(friends, i) end
+  end
+
+  local cardkey = friends[math.ceil(math.random() * #friends)]
+  local newcard = Card(
+    G.title_top.T.x,
+    G.title_top.T.y,
+    G.CARD_W,
+    G.CARD_H,
+    G.P_CARDS.empty,
+    G.P_CENTERS[cardkey],
+    { bypass_discovery_center = true }
+  )
+
+  -- the magic incantations that fix the title screen
+  G.title_top.T.w = G.title_top.T.w + 1.7675
+  G.title_top.T.x = G.title_top.T.x - 0.8
+
+  -- post-processing for the card
+  G.title_top:emplace(newcard)
+  newcard.T.w = newcard.T.w * 1.1 * 1.2
+  newcard.T.h = newcard.T.h * 1.1 * 1.2
+  newcard.no_ui = true
+  newcard:set_sprites(newcard.config.center)
+  newcard.states.visible = false
+  G.E_MANAGER:add_event(Event({
+    trigger = "after",
+    delay = 0,
+    blockable = false,
+    blocking = false,
+    func = function()
+      if change_context == "splash" then
+        newcard.states.visible = true
+        newcard:start_materialize({ G.C.WHITE, G.C.WHITE }, true, 2.5)
+      else
+        newcard.states.visible = true
+        newcard:start_materialize({ G.C.WHITE, G.C.WHITE }, nil, 1.2)
+      end
+      return true
+    end
+  }))
   return ret
 end
 
